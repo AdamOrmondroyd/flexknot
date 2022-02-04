e4d57d026aee5a3a1f4fbb4760b6f05daf922c0b
Ormorod <Adam.Ormondroyd@gmail.com>
2022-02-04 10:29:25 +0000

    Added test_linf.py, also modified test_adaptive_linf to suit new linf

--- tests/test_adaptive_linf.py
+++ tests/test_adaptive_linf.py
@@ -3,11 +3,14 @@ from src.linf import linf, adaptive_linf
 
 
 def test_adaptive_linf():
+    """
+    Test that adaptive_linf returns the same results as linf.
+    """
     x_min = 0.0
-    x_max = 1.0
+    x_max = 6.0
     theta = np.array([2.5, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6])
     theta_n = np.array([0, 1, 1, 2, 2, 3, 3, 6])
-    xs = np.linspace(x_min, x_max)
+    xs = np.linspace(x_min, x_max, 100)
     assert np.all(
         linf(x_min, x_max)(xs, theta_n) == adaptive_linf(x_min, x_max)(xs, theta)
     )
---
+++ tests/test_linf.py
@@ -0,0 +1,22 @@
+import numpy as np
+from src.linf import linf
+
+
+def test_linf():
+    """
+    Test that linf gives the same results as np.interp
+    """
+    x_min = 0
+    x_max = 1
+    N = 8
+    x_nodes = np.sort(np.random.uniform(x_min, x_max, N))
+    y_nodes = np.random.uniform(-10, 10, N + 2)
+    theta = np.zeros(len(x_nodes) + len(y_nodes))
+    theta[1 : 2 * N + 1 : 2] = x_nodes
+    theta[0 : 2 * N + 2 : 2] = y_nodes[:-1]
+    theta[-1] = y_nodes[-1]
+    xs = np.linspace(x_min, x_max, 100)
+    assert np.all(
+        np.interp(xs, np.concatenate(([x_min], x_nodes, [x_max])), y_nodes)
+        == linf(x_min, x_max)(xs, theta)
+    )
