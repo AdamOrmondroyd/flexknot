82ffa7399f0e9d34669f64db9c7a853d2a5e1d28
Ormorod <Adam.Ormondroyd@gmail.com>
2022-02-10 22:31:01 +0000

    Added get_prior() to linf/priors.py

--- linf/priors.py
+++ linf/priors.py
@@ -3,6 +3,11 @@ Priors using linfs.
 
 I need to think carefully how best to abstract away all the weird slicing that
 goes on to make a linf work.
+
+Currently going for n then x_nodes then y_nodes.
+
+Contains UnformPrior, SortedUniformPrior and forced_identifiability_transform()
+which are blatantly stolen from Will Handley's PolyChord
 """
 import numpy as np
 
@@ -37,14 +42,29 @@ class SortedUniformPrior(UniformPrior):
 ## end blatant stealing from polychord
 
 
-def get_prior(x_min, x_max, y_min, y_max, adaptive=False):
+def get_prior(x_min, x_max, y_min, y_max, N_max=None):
+    """
+    Returns a uniform + sorted uniform prior appropriate for a linf.
 
-    if adaptive:
+    N_max = int is the maximum number of nodes to use with an adaptive linf.
 
-        def prior(theta):
-            x_prior = SortedUniformPrior(x_min, x_max)(get_x_nodes_from_theta(theta))
-            y_prior = UniformPrior(y_min, y_max)(get_y_nodes_from_theta(theta))
+    N_max=None will return the prior for a non-adaptive model.
+    """
+    # non-adaptive prior
+    def prior(theta):
+        x_prior = SortedUniformPrior(x_min, x_max)(get_x_nodes_from_theta(theta))
+        y_prior = UniformPrior(y_min, y_max)(get_y_nodes_from_theta(theta))
 
-            # TODO: how best to combine the priors? Interleave or separate?
+        # TODO: how best to combine the priors? Interleave or separate?
+        # for now we're going with separate
+        return np.concatenate((x_prior, y_prior))
 
-    return prior
+    if N_max is None:
+
+        return prior
+
+    def adaptive_prior(theta):
+        n_prior = UniformPrior(0, N_max)(theta[0:1])
+        return np.concatenate((n_prior, prior(theta[1:])))
+
+    return adaptive_prior
