24251698092a74887215ccb308e558bb3c14c5ad
Ormorod <Adam.Ormondroyd@gmail.com>
2022-02-10 22:39:39 +0000

    Moved test_adaptive_linf() into test_linf.py and removed now empty file

--- tests/test_linf.py
+++ tests/test_linf.py
@@ -1,9 +1,10 @@
 """
-Test that linf returns the same results as np.interp.
+Test that linf returns the same results as np.interp, and that
+adaptive_linf() does the same as linf().
 """
 
 import numpy as np
-from linf.linfs import get_linf
+from linf.linfs import get_adaptive_linf, get_linf
 
 
 def test_linf():
@@ -24,4 +25,19 @@ def test_linf():
     assert np.all(
         np.interp(xs, np.concatenate(([x_min], x_nodes, [x_max])), y_nodes)
         == get_linf(x_min, x_max)(xs, theta)
+    )
+
+
+def test_adaptive_linf():
+    """
+    Test that adaptive_linf returns the same results as linf with appropriate arguments.
+    """
+    x_min = 0.0
+    x_max = 6.0
+    theta = np.array([2.5, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6])
+    theta_n = np.array([0, 1, 1, 2, 2, 3, 3, 6])
+    xs = np.linspace(x_min, x_max, 100)
+    assert np.all(
+        get_linf(x_min, x_max)(xs, theta_n)
+        == get_adaptive_linf(x_min, x_max)(xs, theta)
     )
