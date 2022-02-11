4f68f1657bd5ca0637dda7741671eb72fd81bd6d
Ormorod <Adam.Ormondroyd@gmail.com>
2022-02-11 15:37:27 +0000

    Now imports priors from polychord. Not sure why I didn't just do this in the first place! I think I'd be dissuaded from installing polychord after the fiasco with cobaya-installing it

--- linf/priors.py
+++ linf/priors.py
@@ -12,34 +12,7 @@ which are blatantly stolen from Will Handley's PolyChord
 import numpy as np
 
 from linf.helper_functions import get_x_nodes_from_theta, get_y_nodes_from_theta
-
-## copied from polychord, not sure what best way to "steal" this is
-## TODO: figure out better way of stealingß
-class UniformPrior:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def __call__(self, x):
-        return self.a + (self.b - self.a) * x
-
-
-def forced_identifiability_transform(x):
-    N = len(x)
-    t = np.zeros(N)
-    t[N - 1] = x[N - 1] ** (1.0 / N)
-    for n in range(N - 2, -1, -1):
-        t[n] = x[n] ** (1.0 / (n + 1)) * t[n + 1]
-    return t
-
-
-class SortedUniformPrior(UniformPrior):
-    def __call__(self, x):
-        t = forced_identifiability_transform(x)
-        return super(SortedUniformPrior, self).__call__(t)
-
-
-## end blatant stealing from polychord
+from pypolychord.priors import UniformPrior, SortedUniformPrior
 
 
 def get_prior(x_min, x_max, y_min, y_max, N_max=None):
