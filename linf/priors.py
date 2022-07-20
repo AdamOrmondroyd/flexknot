83b7c248c5eeaf53d8a0e171104e2f4bbeb28676
Ormorod <Adam.Ormondroyd@gmail.com>
2022-07-20 15:26:33 +0100

    reorganisation so AdaptiveLinfPrior changes self._x_prior so __call__() is straightforward

--- linf/priors.py
+++ linf/priors.py
@@ -22,8 +22,8 @@ class LinfPrior(UniformPrior):
     """
 
     def __init__(self, x_min, x_max, y_min, y_max):
-        self.x_prior = SortedUniformPrior(x_min, x_max)
-        self.y_prior = UniformPrior(y_min, y_max)
+        self._x_prior = SortedUniformPrior(x_min, x_max)
+        self._y_prior = UniformPrior(y_min, y_max)
 
     def __call__(self, hypercube):
         """
@@ -32,12 +32,12 @@ class LinfPrior(UniformPrior):
         hypercube = [y0, x1, y1, x2, y2, ..., x_(N-2), y_(N-2), y_(N-1)] for N nodes.
         """
         if len(hypercube) > 2:
-            x_prior = self.x_prior(get_x_nodes_from_theta(hypercube, adaptive=False))
+            _x_prior = self._x_prior(get_x_nodes_from_theta(hypercube, adaptive=False))
         else:
-            x_prior = np.array([])
+            _x_prior = np.array([])
         return create_theta(
-            x_prior,
-            self.y_prior(get_y_nodes_from_theta(hypercube, adaptive=False)),
+            _x_prior,
+            self._y_prior(get_y_nodes_from_theta(hypercube, adaptive=False)),
         )
 
 
@@ -49,9 +49,23 @@ class AdaptiveLinfPrior(LinfPrior):
     """
 
     def __init__(self, x_min, x_max, y_min, y_max, N_min, N_max):
-        self.N_prior = UniformPrior(N_min, N_max)
-        self.unused_x_prior = UniformPrior(x_min, x_max)
+        self._N_prior = UniformPrior(N_min, N_max)
         super().__init__(x_min, x_max, y_min, y_max)
+
+        # redefine self._x_prior
+        self.__used_x_prior = self._x_prior
+        self.__unused_x_prior = UniformPrior(x_min, x_max)
+        self.__n_x_nodes = N_max
+        self._x_prior = (
+            lambda hypercube_x: np.concatenate(
+                (
+                    self.__used_x_prior(hypercube_x[: self.__n_x_nodes]),
+                    self.__unused_x_prior(hypercube_x[self.__n_x_nodes :]),
+                )
+            )
+            if self.__n_x_nodes > 0
+            else self.__unused_x_prior(hypercube_x)
+        )
 
     def __call__(self, hypercube):
         """
@@ -61,18 +75,7 @@ class AdaptiveLinfPrior(LinfPrior):
         where Nmax is the greatest allowed value of floor(N).
         """
         prior = np.empty(hypercube.shape)
-        prior[0] = self.N_prior(hypercube[0:1])
-        N = int(prior[0])
-        if N > 0:
-            prior[2:-1:2] = np.concatenate(
-                (
-                    self.x_prior(hypercube[2 : 2 * N - 2 : 2]),
-                    self.unused_x_prior(hypercube[2 : 2 * N - 2 : 2]),
-                )
-            )
-        else:
-            prior[2:-1:2] = self.unused_x_prior(hypercube[2:-1:2])
-        y_prior = self.y_prior(np.concatenate((hypercube[1::2], hypercube[-1:])))
-        prior[1::2] = y_prior[:-1]
-        prior[-1] = y_prior[-1]
+        prior[0] = self._N_prior(hypercube[0:1])
+        self.__n_x_nodes = int(prior[0])
+        prior[1:] = super().__call__(hypercube[1:])
         return prior
